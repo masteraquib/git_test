50bf7a3e4b1aecefce78451c16a3b92f92df92d3
Aquib Raza <masteraquib98@gmail.com>
2024-12-16 11:01:23 +0530

    added/authentication

--- reset-password.py
+++ reset-password.py
@@ -1,7 +1,12 @@
 username = input("Enter username = ")
 password = input("Enter password = ")
-
+is_auth = False
 if username == 'admin' and password == "admin":
     print("You can reset")
+    is_auth =  True
 else:
     print("You cannot reset")
+
+
+if is_auth:
+    print("Now Reset")
